1a2286297b8f17d1e3afbe2851ac9163e776dbc6
luis petek <lmpetek@gmail.com>
2011-08-20 10:31:02 -0300

    primer seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+last_names  = ['Perez','Garcia','Fernandez','Hernandez','Suarez','Gonzalez','Gomez']
+first_names = ['Pedro','Miguel','Mateo'    ,'Juan'     ,'Jose'  ,'Juan'    ,'Pablo']
+piso = 1
+last_names.each do |last_name|
+  dpto = 1
+  first_names.each do |first_name|
+     Functional_unit.create!(:location => 'piso ' piso.to_s + 'dpto ' + dpto.to_s,
+     :owner_id => Owner.create!(:last_name => last_name,:first_name => first_name).id )
+     ++dpto
+  end
+  ++piso 
+end
+
+=begin
+Company.all.each do |company|
+  (1..100).each do |i|
+     Product.create!(:company_id => company.id,:name => "producto n"+i.to_s+company.name, :since => Date.today)
+   end
+end
+=end
