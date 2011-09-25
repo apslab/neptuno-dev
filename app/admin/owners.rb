9b6180f451634ac73c00b95ba4666a42d6aaa97a
luis petek <lmpetek@gmail.com>
2011-09-25 11:14:55 -0300

    owner recent scope

--- app/admin/owners.rb
+++ app/admin/owners.rb
@@ -2,6 +2,7 @@ ActiveAdmin.register Owner do
   
   scope :all, :default => true
   scope :with_cargo
+  scope :recent
   
   scope :modificados_hoy do |owners|
       owners.where("updated_at > ?", Date.today)
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,7 +2,7 @@ class Owner < ActiveRecord::Base
   has_many :functional_units, :class_name => "functional_unite"
   
   scope :with_cargo, where(:last_name => 'Gomez')
-  
+  scope :recent, where('updated_at >= ?', Time.now - 1.week)
   
   def name
     first_name + ' ' + last_name
