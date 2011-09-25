8b81ce146941a0fd77dd834e03108e0a19b9a8c5
luis petek <lmpetek@gmail.com>
2011-09-25 11:10:44 -0300

    i18n and owner scope

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,7 +1,7 @@
 class Owner < ActiveRecord::Base
   has_many :functional_units, :class_name => "functional_unite"
   
-  scope :with_cargo, where("last_name == 'Gomez' ")
+  scope :with_cargo, where(:last_name => 'Gomez')
   
   
   def name
