e388bb751bc8f38516da20e17d869baccff95ffc
luis petek <lmpetek@gmail.com>
2011-09-25 11:19:26 -0300

    remove modificados_hoy

--- app/admin/owners.rb
+++ app/admin/owners.rb
@@ -3,11 +3,7 @@ ActiveAdmin.register Owner do
   scope :all, :default => true
   scope :with_cargo
   scope :recent
-  
-  scope :modificados_hoy do |owners|
-      owners.where("updated_at > ?", Date.today)
-  end
-  
+    
   sidebar :help do
       "Necesita ayuda? envie un Email a ayuda@example.com"
   end
